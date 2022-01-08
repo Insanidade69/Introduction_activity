fbcdc7f04c9cccee6f2084c0b3168391341b4729
Marcelo <61208203+Insanidade69@users.noreply.github.com>
2022-01-08 00:58:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Activity1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+
+            Console.WriteLine("What is radius?");
+            double raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            area = Math.Round(Math.Pow(raio, 2) * 3.14159, 4);
+            Console.WriteLine("The area value is: {0}.", area);
+
+        }
+    }
+}
